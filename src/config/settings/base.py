984acb891aef63dcfe5c35563113363c21f1f9d4
Eleverkhun <Eleverkuhn@users.noreply.github.com>
2025-11-29 18:58:20 +0300

    fix(project): Add alias for local server to django allowed hosts

--- src/config/settings/base.py
+++ src/config/settings/base.py
@@ -29,7 +29,7 @@ SECRET_KEY = env.django_key
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [env.django_host, "localhost"]
+ALLOWED_HOSTS = [env.django_host, "localhost", "127.0.0.1"]
 
 
 # Application definition
