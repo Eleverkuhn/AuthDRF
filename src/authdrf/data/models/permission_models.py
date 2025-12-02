af75b5b3594457f6e57ffea828f341156155056f
Eleverkhun <Eleverkuhn@users.noreply.github.com>
2025-12-02 19:53:09 +0300

    refactor(templates): Add missing navigation

--- src/authdrf/data/models/permission_models.py
+++ src/authdrf/data/models/permission_models.py
@@ -17,6 +17,9 @@ class Role(models.Model):
 
     permissions = models.ManyToManyField(Permission, related_name="roles")
 
+    def __str__(self) -> str:
+        return self.title
+
     def __repr__(self) -> str:
         return self.title
 
